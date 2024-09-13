44abf2173278761a5ade2c1a004f083f0c28bf84
HildaReplika <hilda@replikatech.com>
2024-09-13 16:53:00 +0530

    home services ui development done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
-import { HomeBanner, HomeCompanyHistory } from "@/components/home";
+import {
+  HomeBanner,
+  HomeCompanyHistory,
+  HomeOurServices,
+} from "@/components/home";
 
 export default function Home() {
   return (
     <div className="h-full">
       <HomeBanner />
       <HomeCompanyHistory />
+      <HomeOurServices />
     </div>
   );
 }
--- src/components/magicui/index.ts
+++ src/components/magicui/index.ts
@@ -1,3 +1,4 @@
 export * from "./shine-border";
 export * from "./cool-mode";
 export * from "./fade-text";
+export * from "./shimmer-button";
